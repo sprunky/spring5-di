5875a9d8096b3b796f8f2360eca4cce324377612
jonathan.garrido <jonathan.garrido@incentro.com>
2021-11-08 15:54:59 +0100

    Initial commit

---
+++ src/main/java/com/sprunky/spring5di/Spring5DiApplication.java
@@ -0,0 +1,13 @@
+package com.sprunky.spring5di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5DiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5DiApplication.class, args);
+    }
+
+}
